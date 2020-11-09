4a929ca9d7fb009bbf5fdfd9b46dc52fc38c63bd
Adrien <adrien.3100@gmail.com>
2020-11-09 12:36:23 +0100

    add step 01_ssh_key

---
+++ images/01_ssh_key/ssh_key.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "-h" ]; then
+  echo "Usage: $0 password user target"
+  exit
+fi
+
+if [ -e ~/.ssh/id_rsa ]; then
+  rm ~/.ssh/id_rsa*
+fi
+
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
+
+sshpass -p "$1" ssh-copy-id -o StrictHostKeyChecking=no "$2"@"$3"
