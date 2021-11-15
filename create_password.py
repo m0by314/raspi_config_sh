b3a930b9f883fe9d174b6af93db1d7bf4b2a6ad4
Adrien <adrien.3100@gmail.com>
2021-11-15 16:27:06 +0100

    ansible image run playbook

--- create_password.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from passlib.hash import sha512_crypt
-import getpass
-
-print(sha512_crypt.using(rounds=5000).hash(getpass.getpass()))
--- images/ansible/run_ansible.sh
+++ images/ansible/run_ansible.sh
@@ -5,18 +5,48 @@ ansible-galaxy install m0by314.ansible_raspberry_pi_config
 
 # ansible vault
 vault_key_file="/etc/ansible/vault_key_file"
-role_vars_file="/etc/ansible/vars/vault.yml"
+vault_vars_file="/etc/ansible/playbook/vars/user_pwd.yml"
 
-echo -n Password to encrypt:
-read -s password
-echo
+# Ask password for the new pi user
+i=3
+while [ $i -gt 0 ]
+do
+  echo -n Password for the new pi user:
+  read -s password
+  echo
 
+  echo -n Confirm password:
+  read -s confirm_password
+  echo
+
+  ((i--))
+  if [ "$password"x == ""x ] || [ "$password"x != "$confirm_password"x ]; then
+      echo "The password confirmation does not match"
+      if [ "$i" == 0 ]; then
+        exit 1
+      fi
+  else
+    break
+  fi
+done
+
+# create vault key file
 if [ ! -e $vault_key_file ]; then
   openssl rand -base64 512 | xargs > $vault_key_file
 fi
 
-echo >> $role_vars_file
-ansible-vault encrypt_string --vault-password-file $vault_key_file $password --name 'rasp_user_password' >> $role_vars_file
+# create vault vars file
+echo >> $vault_vars_file
+
+# encrypted password to sha512
+encrypted_pwd=$(python3 /usr/local/bin/encrypt_password.py "$password")
+
+# write encrypted password into user_pwd.yml
+echo "rasp_user_encrypted_password: ${encrypted_pwd}" > $vault_vars_file
+
+# write encrypted vault variable
+ansible-vault encrypt_string --vault-password-file $vault_key_file "$password" --name 'rasp_user_password' >> $vault_vars_file
+
 
 # launch playbook
 ansible-playbook playbook/config.yml --vault-password-file vault_key_file
